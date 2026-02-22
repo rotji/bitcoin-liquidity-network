e57f31c84b9b26a83661896bf0f8fb8476e89c55
rotji <starrotji@gmail.com>
2026-02-22 20:25:28 +0100

    Implement react-router-dom routing with <Routes> and <Route> for proper navigation

--- frontend/bitcoin liquidity network/src/App.tsx
+++ frontend/bitcoin liquidity network/src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
 import Dashboard from './pages/Dashboard';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
